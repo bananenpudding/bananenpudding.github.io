292534945dcd33c4cbb2aa17d0e5b84d3434493b
bananenpudding <abelont1@gmail.com>
2017-07-12 22:36:04 +0200

    Update host.lua

--- lua/switch/host.lua
+++ lua/switch/host.lua
@@ -1,56 +1,107 @@
-local leftName = "me"
-local rightName = "base"
-local topName = "kelder"
-local bottomName = "mars"
+-------------------CONFIGURATION-------------------
+
+-- Host
+
+-- Modem channel - must be between 0 and 65535
+
+local channel = 0
+
+
+-- Modem side
+
+local modemSide = "bottom"
+
+
+-- Give the switches the name of your choice,
+-- or set to "" to disable
+
+local switch1Name = "sw1"
+local switch2Name = "sw2"
+local switch3Name = "sw3"
+local switch4Name = "sw4"
+local switch5Name = "sw5"
+
+
+-- Switch sides
+
+local switch1Side = "left"
+local switch2Side = "right"
+local switch3Side = "front"
+local switch4Side = "back"
+local switch5Side = "top"
+
+
+-- Default switch states
+
+local switch1On = false
+local switch2On = false
+local switch3On = false
+local switch4On = false
+local switch5On = false
+
+----------------END OF CONFIGURATION---------------
+
+function setTextColorIfSupported(color)
+  if term.isColor() then
+    term.setTextColor(color)
+  end
+end
 
 local on = false
 local err = false
 
-local leftOn = true
-local rightOn = true
-local topOn = true
-local bottomOn = true
-
-modem = peripheral.wrap("back")
-modem.open(300)
+modem = peripheral.wrap(modemSide)
+modem.open(channel)
 while true do
-  redstone.setOutput("left", leftOn)
-  redstone.setOutput("right", rightOn)
-  redstone.setOutput("top", topOn)
-  redstone.setOutput("bottom", bottomOn)
+  redstone.setOutput(switch1Side, switch1On)
+  redstone.setOutput(switch2Side, switch2On)
+  redstone.setOutput(switch3Side, switch3On)
+  redstone.setOutput(switch4Side, switch4On)
+  redstone.setOutput(switch5Side, switch5On)
   event, modemSide, senderChannel, replyChannel, message, senderDistance = os.pullEvent("modem_message")
-  if message == leftName then
-    leftOn = not leftOn
-    on = leftOn
-  elseif message == rightName then
-    rightOn = not rightOn
-    on = rightOn
-  elseif message == topName then
-    topOn = not topOn
-    on = topOn
-  elseif message == bottomName then
-    bottomOn = not bottomOn
-    on = bottomOn
+  if message == "" then
+    errNoInput = true
+  elseif message == switch1Name then
+    switch1On = not switch1On
+    on = switch1On
+  elseif message == switch2Name then
+    switch2On = not switch2On
+    on = switch2On
+  elseif message == switch3Name then
+    switch3On = not switch3On
+    on = switch3On
+  elseif message == switch4Name then
+    switch4On = not switch4On
+    on = switch4On
+  elseif message == switch5Name then
+    switch5On = not switch5On
+    on = switch5On
   else
-    err = true
+    errNotExist = true
   end
-  if err then
-    modem.transmit(301, 300, "Switch '" .. message .. "' was entered but does not exist")
-    term.setTextColor(colors.red)
-    print("[" .. senderDistance .. " bl] Switch '" .. message .. "' was entered but does not exist")
-    term.setTextColor(colors.white)
-    err = false
+  if errNotExist then
+    modem.transmit(replyChannel, channel, "Switch '" .. message .. "' does not exist. To exit, type exit.")
+    setTextColorIfSupported(colors.orange)
+    print("[ch " .. replyChannel .. ", " .. senderDistance .. " bl] Switch '" .. message .. "' was entered but does not exist.")
+    setTextColorIfSupported(colors.white)
+    errNotExist = false
+  elseif errNoInput then
+    modem.transmit(replyChannel, channel, "Enter a switch name to switch or type exit to exit.")
+    setTextColorIfSupported(colors.orange)
+    print("[ch " .. replyChannel .. ", " .. senderDistance .. " bl] There was no switch entered.")
+    setTextColorIfSupported(colors.white)
+    errNoInput = false
   else
     if on then
-      modem.transmit(301, 300, "Switch '" .. message .. "' is switched ON.")
-      term.setTextColor(colors.green)
-      print("[" .. senderDistance .. " bl] Switch '" .. message .. "' is switched ON.")
-      term.setTextColor(colors.white)
+      modem.transmit(replyChannel, channel, "Switch '" .. message .. "' is switched ON.")
+      setTextColorIfSupported(colors.green)
+      print("[ch " .. replyChannel .. ", " .. senderDistance .. " bl] Switch '" .. message .. "' is switched ON.")
+      setTextColorIfSupported(colors.white)
     else
-      modem.transmit(301, 300, "Switch '" .. message .. "' is switched OFF.")
-      term.setTextColor(colors.red)
-      print("[" .. senderDistance .. " bl] Switch '" .. message .. "' is switched OFF.")
-      term.setTextColor(colors.white)
+      modem.transmit(replyChannel, channel, "Switch '" .. message .. "' is switched OFF.")
+      setTextColorIfSupported(colors.red)
+      print("[ch " .. replyChannel .. ", " .. senderDistance .. " bl] Switch '" .. message .. "' is switched OFF.")
+      setTextColorIfSupported(colors.white)
     end
   end
 end
